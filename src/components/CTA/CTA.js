499b88582412974ede8fc3614b4eee87a23e3ce6
Razvan Spatariu <razvanspatariu@gmail.com>
2022-11-25 20:56:46 +0000

    Add CTA component

---
+++ src/components/CTA/CTA.js
@@ -0,0 +1,49 @@
+import * as React from "react"
+import PropTypes from 'prop-types';
+import classNames from "classnames";
+import { Link } from "gatsby"
+import { cta, ctaPrimary, ctaSecondary, contentWrapper, content, icon, iconWrapper } from "./CTA.module.css"
+
+const CTA = ({ children, importance, to, hasArrowIcon }) => {
+  const classes = classNames(
+    cta,
+    {
+      [ ctaPrimary ]: importance === 'primary',
+      [ ctaSecondary ]: importance === 'secondary',
+    }
+  )
+  return (
+    <Link className={classes} to={to}>
+      <span className={contentWrapper}>
+        <span className={content}>
+          {children}
+        </span>
+
+        {
+          hasArrowIcon && (
+            <span className={iconWrapper}>
+              <svg className={icon} width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.08301 11L20.9163 11" stroke="#FE5252" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M11 1.08301L20.9167 10.9997L11 20.9163" stroke="#FE5252" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </span>
+          )
+        }
+      </span>
+    </Link>
+  )
+}
+
+CTA.propTypes = {
+  children: PropTypes.string.isRequired,
+  hasArrowIcon: PropTypes.bool,
+  importance: PropTypes.oneOf(["primary", "secondary"]),
+  to: PropTypes.string.isRequired,
+}
+
+CTA.defaultProps = {
+  hasArrowIcon: false,
+  importance: "primary",
+}
+
+export { CTA }
---
+++ src/components/CTA/CTA.test.js
@@ -0,0 +1,87 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import { CTA } from "./CTA"
+
+describe("CTA", () => {
+  describe("when a children prop is passed", () => {
+    test("renders the children prop as content", () => {
+      const { container} = render(
+        <CTA to="/test">Click Me!</CTA>
+      )
+      expect(container.textContent).toBe("Click Me!")
+    })
+  })
+
+  describe("when a to prop is passed", () => {
+    test("adds it as an href to the anchor", () => {
+      const { container} = render(
+        <CTA to="/test">Click Me!</CTA>
+      )
+      expect(container.firstChild.getAttribute("href")).toBe("/test")
+    })
+  })
+
+  describe("when importance is not passed", () => {
+    test("has the default primary importance", () => {
+      const { container} = render(
+        <CTA to="/test">Click Me!</CTA>
+      )
+      expect(container.firstChild.classList.contains("ctaPrimary")).toBeTruthy()
+    })
+  })
+
+  describe("when importance is passed", () => {
+    describe("and the value is primary", () => {
+      test("has the primary importance class", () => {
+        const { container} = render(
+          <CTA to="/test" importance="primary">Click Me!</CTA>
+        )
+        expect(container.firstChild.classList.contains("ctaPrimary")).toBeTruthy()
+      })
+    })
+
+    describe("and the value is secondary", () => {
+      test("has the secondary importance class", () => {
+        const { container} = render(
+          <CTA to="/test" importance="secondary">Click Me!</CTA>
+        )
+        expect(container.firstChild.classList.contains("ctaSecondary")).toBeTruthy()
+      })
+    })
+  })
+
+  describe("when hasArrowIcon is not passed", () => {
+    test("does not render the arrow icon", () => {
+      const { container } = render(
+        <CTA to="/test">Click Me!</CTA>
+      )
+
+      const icon = container.getElementsByClassName("icon")
+      expect(icon.length).toBe(0)
+    })
+  })
+
+
+  describe("when hasArrowIcon is passed as false", () => {
+    test("does not render the arrow icon", () => {
+      const { container } = render(
+        <CTA to="/test" hasArrowIcon={false}>Click Me!</CTA>
+      )
+
+      const icon = container.getElementsByClassName("icon")
+      expect(icon.length).toBe(0)
+    })
+  })
+
+  describe("when hasArrowIcon is passed as true", () => {
+    test("renders the arrow icon", () => {
+      const { container } = render(
+        <CTA to="/test" hasArrowIcon>Click Me!</CTA>
+      )
+
+      const icon = container.getElementsByClassName("icon")
+      expect(icon.length).toBe(1)
+    })
+  })
+})
+
