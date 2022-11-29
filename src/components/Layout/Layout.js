193e29d0397cba3232b6324fa887984c8e655b92
Razvan Spatariu <razvanspatariu@gmail.com>
2022-11-29 15:53:11 +0000

    Move the Start your journey section to the Layout so it appears on all pages

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,24 @@
 import * as React from "react"
 import "normalize.css"
 import PropTypes from 'prop-types';
+import { CTA } from "../CTA"
 import { Header } from "../Header";
+import { Heading } from "../Heading"
+import { PageContainer } from "../PageContainer"
+import { SectionWrapper } from "../SectionWrapper"
 import "./Layout.module.css"
 
 const Layout = ({ children, headerDarkTheme, headerIsOverlapping }) => (
   <div className="liftr">
     <Header theme={headerDarkTheme ? "dark" : null} overlap={headerIsOverlapping} />
     {children}
+
+    <PageContainer isNarrow>
+      <SectionWrapper isCenter>
+        <Heading theme={2} importance={2}>Start your journey with Liftr</Heading>
+        <CTA to="/" hasArrowIcon>Ship products faster</CTA>
+      </SectionWrapper>
+    </PageContainer>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
-import { CTA } from "../components/CTA"
-import { Heading } from "../components/Heading/Heading"
+import { Heading } from "../components/Heading"
 import { HomepageHero } from "../components/HomepageHero/HomepageHero"
 import { Layout } from "../components/Layout"
 import { PageContainer } from "../components/PageContainer"
@@ -14,13 +13,6 @@ const IndexPage = () => {
       <SectionWrapper isCenter hasAlternativeFont>
         <Heading theme={2} importance={2}>Why Liftr</Heading>
         <p>Liftr was launched to rework the product development agency model and make it relevant for the entrepreneurs and businesses of tomorrow in a post pandemic world.</p>
-      </SectionWrapper>
-    </PageContainer>
-
-    <PageContainer isNarrow>
-      <SectionWrapper isCenter>
-        <Heading theme={2} importance={2}>Start your journey with Liftr</Heading>
-        <CTA to="/" hasArrowIcon>Ship products faster</CTA>
       </SectionWrapper>
     </PageContainer>
    </Layout>
