1da2e96eeda31694030d55991aa8ffa61437f2ae
Razvan Spatariu <razvanspatariu@gmail.com>
2022-11-29 15:42:09 +0000

    Add the start your journey section to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import * as React from "react"
+import { CTA } from "../components/CTA"
+import { Heading } from "../components/Heading/Heading"
 import { HomepageHero } from "../components/HomepageHero/HomepageHero"
 import { Layout } from "../components/Layout"
+import { PageContainer } from "../components/PageContainer"
+import { SectionWrapper } from "../components/SectionWrapper"
 
 const IndexPage = () => {
   return (
    <Layout headerDarkTheme headerIsOverlapping>
     <HomepageHero />
+    <PageContainer>
+      <SectionWrapper isCenter>
+        <Heading theme={2} importance={2}>Start your journey with Liftr</Heading>
+        <CTA to="/" hasArrowIcon>Ship products faster</CTA>
+      </SectionWrapper>
+    </PageContainer>
    </Layout>
   )
 }
