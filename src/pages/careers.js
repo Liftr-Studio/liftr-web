d575ef27d1b2f6e99ce4190358d21f7535a77a74
Razvan Spatariu <razvanspatariu@gmail.com>
2022-12-02 19:30:53 +0000

    Add careers page

---
+++ src/pages/careers.js
@@ -0,0 +1,19 @@
+import classNames from "classnames"
+import * as React from "react"
+import { CTA } from "../components/CTA"
+import { Heading } from "../components/Heading"
+import { Layout } from "../components/Layout"
+import { PageContainer } from "../components/PageContainer"
+import { SectionWrapper } from "../components/SectionWrapper"
+
+const CareersPage = () => {
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
+
+export default CareersPage
+
+export const Head = () => <title>Liftr Studio | Careers</title>
