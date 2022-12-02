addddef1db1f934ba0cfd0a0ea8f05d937df09d5
Razvan Spatariu <razvanspatariu@gmail.com>
2022-12-02 19:32:25 +0000

    Add Life@Liftr section on the careers page

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -5,11 +5,23 @@ import { Heading } from "../components/Heading"
 import { Layout } from "../components/Layout"
 import { PageContainer } from "../components/PageContainer"
 import { SectionWrapper } from "../components/SectionWrapper"
+import {
+  utilityCustomUL,
+} from "../components/Layout/Layout.module.css"
+
+import {
+  headingLifeAtLiftr,
+} from "./careers.module.css"
 
 const CareersPage = () => {
   return (
     <Layout>
-
+      <PageContainer>
+        <SectionWrapper hasAlternativeFont isCenter>
+          <Heading theme={2} extraClass={headingLifeAtLiftr}>Life @Liftr</Heading>
+          <p>We are setting up our first development centre in Romania. We are a remote first company but, occasionally, we meet IRL in the Cluj area to celebrate success, plan ahead and build a sense of community.</p>
+        </SectionWrapper>
+      </PageContainer>
     </Layout>
   )
 }
