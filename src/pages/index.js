ae0ec781e976358219ae78513f3d304e2e817e57
Razvan Spatariu <razvanspatariu@gmail.com>
2022-12-04 14:54:31 +0000

    Add images for Media section on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,11 @@ import { Layout } from "../components/Layout"
 import { Media } from "../components/Media/Media"
 import { PageContainer } from "../components/PageContainer"
 import { SectionWrapper } from "../components/SectionWrapper"
-import tailoredProducts from "../images/media/tailored-products.svg"
+import foundersImage from "../images/media/founders.jpg"
+import knowHowImage from "../images/media/know-how.jpg"
+import powerfulNetworksImage from "../images/media/powerful-networks.jpg"
+import tailoredProductsImage from "../images/media/tailored-products.jpg"
+import verticalOwnershipImage from "../images/media/vertical-ownership.jpg"
 import {
   utilityCustomUL,
   utilityCustomContrastUL,
@@ -77,23 +81,23 @@ const IndexPage = () => {
 
       <PageContainer>
         <SectionWrapper>
-          <Media image={tailoredProducts}>
+          <Media image={tailoredProductsImage}>
             <Heading importance={2} theme={3}>Tailored product</Heading>
             <p>We fully embed our product engineering teams in your business so they can think, breathe and dream alongside your own team. We will grow a team that becomes an expert at what you do, and help you in-house its skills over time.</p>
           </Media>
-          <Media image={tailoredProducts} flip>
+          <Media image={verticalOwnershipImage} flip>
             <Heading importance={2} theme={3}>Vertical ownership</Heading>
             <p>We design it together, we code it, we fix it, we ship it, we grow it. It’s always going to be your product, but also our baby.</p>
           </Media>
-          <Media image={tailoredProducts}>
+          <Media image={knowHowImage}>
             <Heading importance={2} theme={3}>Serious know-how</Heading>
             <p>We look for people with serious mastery that can work collaboratively directly with founders and ship great product at pace.</p>
           </Media>
-          <Media image={tailoredProducts} flip>
+          <Media image={powerfulNetworksImage} flip>
             <Heading importance={2} theme={3}>Powerful networks</Heading>
             <p>We have a range of investors, entrepreneurs and executives that we stay in close contact with. We can make the right introductions as they are needed.</p>
           </Media>
-          <Media image={tailoredProducts}>
+          <Media image={foundersImage}>
             <Heading importance={2} theme={3}>Experienced founders</Heading>
             <p>We have spent over 15 years working with, contributing to and helping scale high growth companies. We know what breakneck growth looks like because we lived through it ourselves.</p>
 
