def47aebc3315e281ff8990823d0bf8a5c7ef365
Razvan Spatariu <razvanspatariu@gmail.com>
2022-12-04 16:31:16 +0000

    Add favicon and other icons through a manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,28 @@ module.exports = {
     title: `liftr`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: "src/images/icon.png",
+        name: "Liftr Studio",
+        short_name: "Liftr Studio",
+        start_url: `/`,
+        background_color: "rgb(254, 82, 82)",
+        theme_color: "rgb(147, 198, 0)",
+      },
+    },
+  ]
 };
